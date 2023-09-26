1dff92a7334a1d8d6f6d3b3c0e7358cd7f775a20
ADNANLH <adnanharrak5@gmail.com>
2023-09-26 10:42:43 +0100

    add

--- Prototype1/db.sql
+++ Prototype1/db.sql
@@ -1,26 +1,16 @@
 CREATE TABLE personne (
     id INT AUTO_INCREMENT PRIMARY KEY,
     Nom VARCHAR(255) NOT NULL,
-    Cne VARCHAR(255) NOT NULL
+    Cne VARCHAR(255) NOT NULL,
+    Type VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE ville (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Ville VARCHAR(255) NOT NULL,
-    personneId INT,
-    FOREIGN KEY (personneId) REFERENCES personne(id)
-);
 
 -- Insert data into personne table
 INSERT INTO personne (Nom, CNE) VALUES
-    ('bakkali', '12345'),
-    ('harrak', '67890'),
-    ('Alaoui', '54321');
-    ('adnan', '543244');
+    ('bakkali', '12345', 'stagiaire'),
+    ('harrak', '67890', 'stagiaire'),
+    ('Alaoui', '54321', 'stagiaire'),
+    ('adnan', '543244', 'stagiaire');
 
 -- Insert data into ville table
-INSERT INTO ville (Ville, personneId) VALUES
-    ('tanger', 1),
-    ('tetouan', 2),
-    ('larach', 3);
-    ('rabat', 4);
